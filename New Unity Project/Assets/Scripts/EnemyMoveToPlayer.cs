fc758f482a83f7e37b7ecb73322ddc6cd922d5d8
AlexB <alexandrubugnar@yahoo.com>
2019-04-15 16:33:30 +0200

    genesis

---
+++ New Unity Project/Assets/Scripts/EnemyMoveToPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveToPlayer : MonoBehaviour {
+
+    private Vector2 target;
+    private Vector2 position;
+    public float speed;
+    public int health = 1;
+    private float direction;
+    private float time = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("PlayerCharacter");
+        target = player.transform.position;
+        position = gameObject.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            ScoreScript.ScoreValue += 1;
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "PlayerSpell")
+        {
+          
+            health--;
+        }
+    }
+}

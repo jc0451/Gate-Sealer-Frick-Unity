53a2752847dcfcfac460256a1860fd41aa8a0868
Maherj <Maherj.jaber23@gmail.com>
2019-04-25 17:11:27 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ New Unity Project/Assets/Scripts/player2anim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2anim : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            anim.SetBool("IsTrigger", true);
+        }
+        else
+        {
+            anim.SetBool("IsTrigger", false);
+        }
+
+
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/playeranim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playeranim : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) )
+        {
+            anim.SetBool("IsTrigger", true);
+        }
+        else
+        {
+            anim.SetBool("IsTrigger", false);
+        }
+
+
+
+        if (Input.GetKey(KeyCode.E) )
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+	}
+}

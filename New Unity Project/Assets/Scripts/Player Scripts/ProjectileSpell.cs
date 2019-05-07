60b3e417b185f07a95d8bc27e8c1154181acdd48
AlexB <alexandrubugnar@yahoo.com>
2019-05-07 14:32:49 +0200

    projectile spell

---
+++ New Unity Project/Assets/Scripts/Player Scripts/ProjectileSpell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpell : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float speed;
+    Vector2 position;
+    Vector2 limit;
+    
+    // Use this for initialization
+    void Start () {
+        rb.velocity = new Vector2(0.0f, speed);
+        limit = new Vector2 (0.0f, 4.0f);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        position = gameObject.transform.position;
+        if (limit.y <= position.y)
+        {
+            Destroy(gameObject);
+        }
+        
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

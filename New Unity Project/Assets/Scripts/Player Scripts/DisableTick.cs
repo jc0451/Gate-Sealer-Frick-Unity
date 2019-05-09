63294656390b8cc85f558bbac6ec9ed04fb9a53a
AlexB <alexandrubugnar@yahoo.com>
2019-05-09 15:56:31 +0200

    Spells V3

---
+++ New Unity Project/Assets/Scripts/Player Scripts/DisableTick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTick : MonoBehaviour {
+    public float time = 0.5f;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0.0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Player Scripts/Explosion2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion2 : MonoBehaviour {
+
+    public float time = 0.5f;
+    public float currenttime;
+    public int nr;
+    public int childs;
+    // Use this for initialization
+    void Start () {
+        currenttime = time;
+        nr = 0;
+        childs = transform.childCount;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+           
+        currenttime -= Time.deltaTime;
+        if (currenttime <= 0.0f && nr < childs)
+         {
+            transform.GetChild(nr).gameObject.SetActive(true);
+            currenttime = time;
+            nr+=1;
+         }
+        if (currenttime < -2f)
+        {
+            Destroy(gameObject);
+        }
+           
+        
+    }
+    void activation(int nr)
+    {
+        transform.GetChild(nr).gameObject.SetActive(true);
+        currenttime = time;
+    }
+}

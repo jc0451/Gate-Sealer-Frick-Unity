3160c70d348b9ac1445605f351a2fdeb61ea8ce9
MaherJ <Maherj.jaber23@gmail.com>
2019-05-09 17:44:21 +0200

    Floating Damage (code)
    we just need to decide about the balancing so i can wire it to the spells and their damage.

---
+++ New Unity Project/Assets/Scripts/FloatingDmg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingDmg : MonoBehaviour
+{
+    [SerializeField] private Transform floatingDamage;
+
+    void Start()
+    {
+
+        DamagePopup.Create(floatingDamage,Vector3.zero, 300);
+    }
+
+    
+}

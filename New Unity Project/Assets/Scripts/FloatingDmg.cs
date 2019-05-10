7c9c0e3fc480a9f53e91624682363ee1ca7cf12a
MaherJ <Maherj.jaber23@gmail.com>
2019-05-10 12:04:36 +0200

    popup on mouse key to test

--- New Unity Project/Assets/Scripts/FloatingDmg.cs
+++ New Unity Project/Assets/Scripts/FloatingDmg.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class FloatingDmg : MonoBehaviour
 {
     [SerializeField] private Transform floatingDamage;
+    Vector3 button;
 
     void Start()
     {
 
-        DamagePopup.Create(floatingDamage,Vector3.zero, 300);
+        
     }
 
-    
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            button = Input.mousePosition;
+            button.z = 2.0f;       // we want 2m away from the camera position
+            Vector3 objectPos = Camera.current.ScreenToWorldPoint(button);
+            
+            DamagePopup.Create(floatingDamage, objectPos, 300);
+        }
+    }
 }

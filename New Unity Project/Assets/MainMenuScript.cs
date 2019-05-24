4190d3cb4be87a35cd85950d8a070d51fe470301
Maherj <Maherj.jaber23@gmail.com>
2019-05-24 15:46:51 +0200

    Menu added , Don't pull without making a backup

--- New Unity Project/Assets/MainMenuScript.cs
+++ New Unity Project/Assets/MainMenuScript.cs
@@ -2,19 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private int P1active = 0;
+    private int P2active = 0;
 
-    public void PlayButton()
+    public Sprite[] BackgroundSprites;
+    public Image backgroundImage;
+
+    public static int currentBackground;
+
+    void Start()
     {
-        SceneManager.LoadScene("Main");
+        currentBackground = 0;
+    }
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentBackground = 1;
+            P1active = 1;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            currentBackground = 2;
+            P2active = 1;
+        }
+
+
+
+
+        if (P1active == 1 && P2active == 1)
+        {
+            currentBackground = 3;
+            SceneManager.LoadScene("Main");
+        }
+
+        backgroundImage.sprite = BackgroundSprites[currentBackground];
+
     }
 
-    public void CreditsButton()
-    {
-        SceneManager.LoadScene("Credits");
-    }
+
     public void HighScoreButton()
     {
         SceneManager.LoadScene("HighScore");

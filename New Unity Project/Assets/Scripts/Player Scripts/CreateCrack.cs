71f4dc96965b4227da3dcedb1eb5a2ceb3e78acd
AlexB <alexandrubugnar@yahoo.com>
2019-06-02 17:26:58 +0200

    NUKE + LASER + fixes

--- New Unity Project/Assets/Scripts/Player Scripts/CreateCrack.cs
+++ New Unity Project/Assets/Scripts/Player Scripts/CreateCrack.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CreateCrack : MonoBehaviour {
     public GameObject crack;
-
+    Vector2 pos;
+    float time = 0.2f;
+    float angle;
 	// Use this for initialization
 	void Start () {
-        GameObject crackinst = (GameObject)Instantiate(crack);
-        crackinst.transform.position = transform.position;
+        pos = transform.position;
+        pos.y -= 1;
+        angle = Random.Range(-10f, 10f);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            GameObject crackinst = (GameObject)Instantiate(crack);
+
+            crackinst.transform.position = pos;
+            crackinst.transform.localScale = transform.localScale;
+            crackinst.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            time = 99;
+        }
 		
 	}
 }
